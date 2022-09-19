76cacc59a389f380a8eef8e716fd5c95103b8e05
Jaedson Silva <contacanal798@gmail.com>
2022-09-19 00:39:13 -0300

    adding Home page style

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Icon } from '@iconify/react';
 
 import Header from '../components/Header';
+import './Home.css'
 
 export default function Home() {
     return (
@@ -9,10 +10,12 @@ export default function Home() {
             <Header/>
             <div className="home-container">
                 <h1>Suas <span>tarefas</span> em um só lugar.</h1>
-                <p>Adicione suas tarefas de forma 
-                simples, rápida e organizada.</p>
-                <div className="button-container">
-                    <button>Comece agora <Icon icon="clarity:pencil-line" /></button>
+                <div className="info">
+                    <p>Adicione suas tarefas de forma 
+                    simples, rápida e organizada.</p>
+                    <div className="button-container">
+                        <button>Comece agora <Icon className='pencil-icon' icon="clarity:pencil-line" /></button>
+                    </div>
                 </div>
             </div>
         </>
