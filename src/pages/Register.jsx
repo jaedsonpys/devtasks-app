06be4a6c476260bba2fd2aa682988f9f5e956a60
Jaedson Silva <contacanal798@gmail.com>
2022-09-19 10:56:56 -0300

    adding register inputs to Register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -8,8 +8,13 @@ export default function Register() {
         <>
             <Header/>
             <div className="register-container">
-                <h1>Crie uma nova conta.</h1>
-                <Input type='text' placeholder='Seu email' icon='carbon:email'/>
+                <div className="title">
+                    <h1>Crie uma nova conta.</h1>
+                </div>
+                <div className="form">
+                    <Input type='text' placeholder='Seu email' icon='carbon:email'/>
+                    <Input type='password' placeholder='Crie uma senha' icon='carbon:password'/>
+                </div>
             </div>
         </>
     )
