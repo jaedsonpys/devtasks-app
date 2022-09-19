b6e74f440272fd36f07b734938b8af12d5330f4a
Jaedson Silva <contacanal798@gmail.com>
2022-09-19 10:31:00 -0300

    creating Register page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import Home from './pages/Home'
+import Register from './pages/Register'
 
 export default function App() {
     return (
         <Router>
             <Routes>
                 <Route path='/' element={<Home/>}/>
+            </Routes>
+            <Routes>
+                <Route path='/register' element={<Register/>}/>
             </Routes>
         </Router>
     )
---
+++ src/pages/Register.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Header from '../components/Header';
+
+export default function Register() {
+    return (
+        <>
+            <Header/>
+            <div className="register-container">
+                <h1>Crie uma nova conta.</h1>
+            </div>
+        </>
+    )
+}
