d55b7abc151cfad0f20df60e8639f6ca6629d8e9
Jaedson Silva <contacanal798@gmail.com>
2022-09-18 23:33:56 -0300

    import Header.css to Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import { Link, Navigate } from 'react-router-dom';
 
+import './Header.css'
+
 export default function Header() {
     return (
-        <div className="header-container">
+        <header className="header-container">
             <nav>
-                <Link className='logo-link' to='/'>devtasks</Link>
+                <Link className='logo-link' to='/'>
+                    <h1>devtasks</h1>
+                </Link>
                 <div className="auth-container">
-                    <Link className='login-link' to='/'>login</Link>
-                    <Link className='register-link' to='/'>registre-se</Link>
+                    <Link className='login-link' to='/login'>
+                        <h2>login</h2>
+                    </Link>
+                    <Link className='register-link' to='/register'>
+                        <h2>registre-se</h2>
+                    </Link>
                 </div>
             </nav>
-        </div>
+        </header>
     )
 }
