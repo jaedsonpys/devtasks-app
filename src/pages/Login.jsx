56198c2fb88a800e018c7c10815e48973c918f5b
Jaedson Silva <contacanal798@gmail.com>
2022-09-20 17:15:00 -0300

    adding Login page and page style

---
+++ src/pages/Login.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import Header from '../components/Header';
+import Input from '../components/Input';
+import Button from '../components/Button';
+
+import './Login.css'
+
+export default function Register() {
+    return (
+        <>
+            <Header/>
+            <div className="login-container">
+                <div className="title">
+                    <h1>Faça login na sua conta</h1>
+                </div>
+                <div className="form">
+                    <Input type='text' placeholder='Seu email' icon='carbon:email'/>
+                    <Input type='password' placeholder='Sua senha' icon='carbon:password'/>
+                    <Button text='Entrar na conta' onClick={() => console.log('register')}/>
+                </div>
+            </div>
+        </>
+    )
+}
