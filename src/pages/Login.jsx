b18f639a235f517ac6a39db52e0d69ed5e0a424e
Jaedson Silva <contacanal798@gmail.com>
2022-10-01 13:11:15 -0300

    consuming api to perform user login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,31 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import Header from '../components/Header';
 import Input from '../components/Input';
 import Button from '../components/Button';
 
+import api from '../services/api';
+
 import './Login.css'
 
-export default class Register extends React.Component {
-    componentDidMount() {
-        document.title = 'Entre em sua conta DevTasks.';
-    };
+export default function Register() {
+    const navigate = useNavigate();
 
-    render() {
-        return (
-            <>
-                <Header/>
-                <div className="login-container">
-                    <div className="title">
-                        <h1>Faça login na sua conta</h1>
-                    </div>
-                    <div className="form">
-                        <Input type='text' placeholder='Seu email' icon='carbon:email'/>
-                        <Input type='password' placeholder='Sua senha' icon='carbon:password'/>
-                        <Button text='Entrar na conta' onClick={() => console.log('register')}/>
-                    </div>
-                </div>
-            </>
-        )
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const registerUser = () => {
+        api
+            .post('/api/register', {
+                email: email,
+                password: password
+            })
+            .then((response) => {
+                if(response.status === 201) {
+                    localStorage.setItem('uAuth', response.data.token);
+                    localStorage.setItem('uEmail', email);
+                    localStorage.setItem('uAuth', password);
+
+                    navigate('/todo');
+                }
+            })
+            .catch((error) => console.error(error));
     }
+
+    return (
+        <>
+            <Header/>
+            <div className="login-container">
+                <div className="title">
+                    <h1>Faça login na sua conta</h1>
+                </div>
+                <div className="form">
+                    <Input type='text' onChange={(e) => setEmail(e.target.value)} value={email} placeholder='Seu email' icon='carbon:email'/>
+                    <Input type='password' onChange={(e) => setPassword(e.target.value)} value={password} placeholder='Sua senha' icon='carbon:password'/>
+                    <Button text='Entrar na conta' onClick={registerUser}/>
+                </div>
+            </div>
+        </>
+    )
 }
