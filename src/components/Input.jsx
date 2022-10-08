064cfc2c1be25e4cc097b4524d6c75f66f8ce84f
Jaedson Silva <contacanal798@gmail.com>
2022-10-08 15:23:38 -0300

    getting onChange props in Input component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,13 +3,13 @@ import { Icon } from '@iconify/react';
 
 import './Input.css'
 
-export default function Input({ type, placeholder, icon }) {
+export default function Input({ onChange, type, placeholder, icon }) {
     return (
         <div className="input-box">
             <div className="icon-box">
                 <Icon icon={icon} className='icon'/>
             </div>
-            <input type={type} placeholder={placeholder}/>
+            <input onChange={onChange} type={type} placeholder={placeholder}/>
         </div>
     )
 }
