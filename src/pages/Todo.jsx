4be351ea4fb85362679b3015ec835b41df986289
Jaedson Silva <contacanal798@gmail.com>
2022-10-09 16:41:20 -0300

    usign function instead of class to Todo component

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -5,34 +5,28 @@ import Task from '../components/Task';
 
 import './Todo.css'
 
-export default class Todo extends React.Component {
-    componentDidMount() {
-        document.title = 'Sua lista de tarefas';
-    }
-
-    render() {
-        return (
-            <div className="todo-container">
-                <div className="todo-title">
-                    <h1>Suas tarefas</h1>
-                </div>
-                <form className='add-task-form'>
-                    <div className="add-task-input-box">
-                        <input type="text" placeholder='Adicionar nova tarefa'/>
-                        <div className="icon-box">
-                            <Icon className='add-icon' icon='carbon:add'/>
-                        </div>
-                    </div>
-                </form>
-                <div className="tasks-container">
-
-                </div>
-                <div className="footer-logo">
-                    <a href="/">
-                        <h1>devtasks</h1>
-                    </a>
-                </div>
+export default function Todo(){
+    return (
+        <div className="todo-container">
+            <div className="todo-title">
+                <h1>Suas tarefas</h1>
             </div>
-        )
-    }
+            <form className='add-task-form'>
+                <div className="add-task-input-box">
+                    <input type="text" placeholder='Adicionar nova tarefa'/>
+                    <div className="icon-box">
+                        <Icon className='add-icon' icon='carbon:add'/>
+                    </div>
+                </div>
+            </form>
+            <div className="tasks-container">
+
+            </div>
+            <div className="footer-logo">
+                <a href="/">
+                    <h1>devtasks</h1>
+                </a>
+            </div>
+        </div>
+    )
 }
