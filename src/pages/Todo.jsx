c31c019c958037be8fa895d310a5568ad09486dd
Jaedson Silva <contacanal798@gmail.com>
2022-11-18 12:51:26 -0300

    getting tasks from api

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,5 +1,6 @@
-import { React, useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import { Icon } from '@iconify/react';
+import { useNavigate } from 'react-router-dom';
 
 import Task from '../components/Task';
 import api from '../services/api';
@@ -8,11 +9,25 @@ import './Todo.css';
 
 export default function Todo(){
     const [tasks, setTasks] = useState([]);
+    const navigate = useNavigate();
 
-    const addNewTask = (newTask) => {
-        const taskList = tasks;
-        taskList.push(newTask);
-        setTasks(taskList);
+    useEffect(() => {
+        getTasks();
+    }, []);
+
+    const getTasks = () => {
+        const token = localStorage.getItem('uAuth');
+
+        api
+            .get('/api/tasks', {headers: {'Authorization': `Bearer ${token}`}})
+            .then((response) => {
+                setTasks(response.data);
+            })
+            .catch(({response}) => {
+                if(response.status === 401) {
+                    navigate('/login')
+                }
+            })
     }
 
     return (
@@ -29,7 +44,7 @@ export default function Todo(){
                 </div>
             </form>
             <div className="tasks-container">
-
+                {tasks.map((data) => <Task title={data.name} status={data.status}/>)}
             </div>
             <div className="footer-logo">
                 <a href="/">
