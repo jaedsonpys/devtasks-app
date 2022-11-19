20aef02da1092be7dd55f1a4746bc8bd591b8ff7
Jaedson Silva <contacanal798@gmail.com>
2022-11-19 14:21:06 -0300

    importing style to LoadingAnimation

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './Loading.css'
+
 export default function LoadingAnimation() {
     return (
         <div class="lds-ring"><div></div><div></div><div></div><div></div></div>
