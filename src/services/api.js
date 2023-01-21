d6a310ae94c036c75647ea79fb923db86f7e35b4
Jaedson Silva <contacanal798@gmail.com>
2023-01-21 19:26:48 -0300

    feat: remove "withCredentials" from axios.create()

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_BASE_URL,
-    withCredentials: true
+    baseURL: process.env.REACT_APP_BASE_URL
 });
 
 export default api;
